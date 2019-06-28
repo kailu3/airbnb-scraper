d03897d8c4842b400ebe117d0e78108ae1714eee
Kai Lu <kailu3@sas.upenn.edu>
2019-06-28 08:34:14 -0400

    ran 3 more cycles, did some cleaning

--- cancun.sh
+++ cancun.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-for var in `seq 21 10 991`
+for var in `seq 15 10 995`
 do
 
    lower_bound=$var
    upper_bound=`expr $var + 10`
+
    CITY="Cancun"
-   
    CONJUNCTION="to"
    FORMAT=".json"
    DATA_LOCATION="cancun_data"
@@ -16,9 +16,5 @@ do
    scrapy crawl airbnb -o $filename -a city=$CITY -a price_lb=$lower_bound -a price_ub=$upper_bound
    mv $filename $DATA_LOCATION
 
-#    echo $LOWER_BOUND
-#    echo $UPPER_BOUND
-#    echo $CITY
-#    echo $filename
 done
 
