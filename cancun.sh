e5facfc850942084bc0480a3fe8494de11918253
Kai Lu <kai.lu@shopify.com>
2019-06-29 11:29:51 -0400

    Decided to separate analysis and crawler. Moved data to new repo

--- cancun.sh
+++ cancun.sh
@@ -6,12 +6,14 @@ do
    lower_bound=$var
    upper_bound=`expr $var + 10`
 
+   # Fixed Variables
    CITY="Cancun"
    CONJUNCTION="to"
    FORMAT=".json"
    DATA_LOCATION="cancun_data"
 
    filename="$lower_bound$CONJUNCTION$upper_bound$FORMAT"
+
    # Run scraper on specific range
    scrapy crawl airbnb -o $filename -a city=$CITY -a price_lb=$lower_bound -a price_ub=$upper_bound
    mv $filename $DATA_LOCATION
